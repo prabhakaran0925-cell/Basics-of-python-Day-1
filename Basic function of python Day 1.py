ac330979096056867a0fc8e147776c0089f32c4a
prabhakaran0925-cell <prabhakaran0925@gmail.com>
2026-09-22 13:26:43 +0530

    Add files via upload

---
+++ Basic function of python Day 1.py
@@ -0,0 +1,21 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name= 'Prabha'
+>>> A=name
+>>> print(A)
+Prabha
+>>> 
+>>> 
+>>> 
+>>> 
+>>> type(A)
+<class 'str'>
+>>> 
+>>> 
+>>> age=40
+>>> type(age)
+<class 'int'>
+>>> 
+>>> age= str(40)
+>>> type(age)
+<class 'str'>
